dfa82b0ff8fa2d7e1b345aeb3fc8b8be11a5cc0a
Richard Sundqvist <whisp91@hotmail.com>
2017-11-03 11:06:50 +0100

    E1.3

--- E1/Task3/main.c
+++ E1/Task3/main.c
@@ -5,7 +5,6 @@
 #include <math.h>
 #include <stdlib.h>
 #include "fft_func.h"
-#include "../../utils/utils.h"
 #define PI 3.141592653589
 #define n 250 /*number of timesteps*/
 
@@ -15,55 +14,39 @@ int main ()
 	double dt = 0.1;
 
 	/* Declare and set Iteration parameter, data arrays and frequency */
+	int i;
 	double data[n];
 	double freq[n];
 	double powspec_data[n];
-	double f_values[] = {0, 1, 2};
-	double phi_values[] = {0, PI/2};
-	char info_string[100];
+	double f = 2;
 
 	/* declare file variables */
 	FILE *file1;
 	FILE *file2;	
 
-	int round = 0;
-	for (int i = 0; i < 3; ++i) {
-		double f = f_values[i];
-		for (int j = 0; j < 2; ++j) {
-			double phi = phi_values[j];
-			sprintf(info_string, "_%.2f_%.2f.dat", f, phi);
-
-
-			/* calculate data points */
-			for (int k = 0; k < n; k++)
-				data[i] = cos(2*PI*f*dt*k + phi);
-
-
-			/*Save function values in file*/
-			file1 = fopen(concat("dat/function",info_string),"w");
-			for (int k = 0; k < n; k++)	{
-				fprintf (file1,"%e \t %e \n", k*dt, data[k]);
-			}
-			fclose(file1);
-
-			/* make FFT (powerspectrum) */
-			powerspectrum(data, powspec_data, n);
-			powerspectrum_shift(powspec_data,n);
-			fft_freq_shift(freq, dt, n);
-
-			/*Save powerspectrum data in file */
-			file2 = fopen(concat("dat/powerspectrum",info_string),"w");
-			for (int k = 0; k < n; k++)	{
-				fprintf (file2,"%e \t %e\n", freq[k], powspec_data[k]);
-			}
-
-			fclose(file2);
-
-			printf("round =  %d\n", ++round);
-		}
+	/* calculate data points */
+	for (i = 0; i < n; i++)	{
+		data[i] = cos(2*PI*f*dt*i);
 	}
 
+	/*Save function values in file*/
+	file1 = fopen("function.dat","w");
+	for (i = 0; i < n; i++)	{
+		fprintf (file1,"%e \t %e \n", i*dt, data[i]);
+	}
+	fclose(file1);
 
+	/* make FFT (powerspectrum) */
+	powerspectrum(data, powspec_data, n);
+	powerspectrum_shift(powspec_data,n);
+	fft_freq_shift(freq, dt, n);
+
+	/*Save powerspectrum data in file */
+  file2 = fopen("powerspectrum.dat","w");
+	for (i = 0; i < n; i++)	{
+		fprintf (file2,"%e \t %e\n", freq[i], powspec_data[i]);
+	}
+	
+	fclose(file2);
 	return 0;
 }
-
