f63cd3dfc72a20a7168aad4fe99f82e9d41c6b3a
Richard Sundqvist <whisp91@hotmail.com>
2017-11-03 11:21:20 +0100

    E1.3 matlab figs

--- E1/Task3/plot_powerspectrum.m
+++ E1/Task3/plot_powerspectrum.m
@@ -1,8 +1,8 @@
 % plot the powerspectrum
 % Created by Martin Gren 2014-10-25.
-
+close; % close figs
 % load the data file
-data = importdata('powerspectrum.dat');
+data = importdata('powerspectrum258.dat');
 
 %plot
 figure;
@@ -11,6 +11,6 @@ plot(data(:,1),data(:,2),'-');
 % labels
 xlabel('Frequency','Interpreter','LaTeX');
 ylabel('Powers pectrum','Interpreter','LaTeX');
-
+title('f = 2, \psi = 0, n = 258');
 
 
