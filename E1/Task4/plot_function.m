13033136401cfa5d558720472a26eeaab115b43f
Cheezeburger94 <andi199424@hotmail.com>
2017-11-06 12:31:01 +0100

    task4 matlaab update

--- E1/Task4/plot_function.m
+++ E1/Task4/plot_function.m
@@ -8,13 +8,17 @@ data2 = importdata('powerspectrum.dat');
 %plot
 figure;
 hold on;
+subplot(2,1,1)
 plot(data1(:,1),data1(:,2),'-');
-plot(data2(:,1),data2(:,2),'-.');
-legend('function', 'powahspec');
-
-% labels
 xlabel('$t$','Interpreter','LaTeX');
 ylabel('$h(t)$','Interpreter','LaTeX');
+subplot(2,1,2)
+plot(data2(:,1),data2(:,2),'-.');
+xlabel('$f$','Interpreter','LaTeX');
+ylabel('$Pn$','Interpreter','LaTeX');
+%legend('function', 'powahspec');
+
+
 
 
 
