23aea443fad16fd22d58e2d153a82b51d3c719fd
Richard Sundqvist <ricsun@f7204-16.f7204.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-11-06 12:28:27 +0100

    E1.4: remove fft freq shift

--- E1/Task4/plot_function.m
+++ E1/Task4/plot_function.m
@@ -1,5 +1,6 @@
 % plot the powerspectrum
 % Created by Martin Gren 2014-10-25.
+close all;
 
 % load the data file
 data1 = importdata('function.dat');
@@ -8,9 +9,12 @@ data2 = importdata('powerspectrum.dat');
 %plot
 figure;
 hold on;
+subplot(2, 1, 1);
 plot(data1(:,1),data1(:,2),'-');
+legend('function')
+subplot(2, 1, 2);
 plot(data2(:,1),data2(:,2),'-.');
-legend('function', 'powahspec');
+legend('powerspec');
 
 % labels
 xlabel('$t$','Interpreter','LaTeX');
