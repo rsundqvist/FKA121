d767942c49db66c7bb3fc52a1d16c93918c2f29c
Richard Sundqvist <ricsun@f7203-01.f7203.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-11-06 09:11:51 +0100

    foo

--- E1/Task4/plot_function.m
+++ E1/Task4/plot_function.m
@@ -2,15 +2,15 @@
 % Created by Martin Gren 2014-10-25.
 
 % load the data file
-data1 = importdata('function_phi0.dat');
-data2 = importdata('function_phipihalf.dat');
+data1 = importdata('function.dat');
+data2 = importdata('powerspectrum.dat');
 
 %plot
 figure;
 hold on;
 plot(data1(:,1),data1(:,2),'-');
 plot(data2(:,1),data2(:,2),'-.');
-legend(["\phi = 0", "\phi = \pi/2"]);
+legend('function', 'powahspec');
 
 % labels
 xlabel('$t$','Interpreter','LaTeX');
