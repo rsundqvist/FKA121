345a1a1c0c22b8842c57a2bb4b6ee1f52774e811
Cheezeburger94 <andi199424@hotmail.com>
2017-11-07 15:41:30 +0100

    numbers

--- E1/Task7/plot_powerspectrum.m
+++ E1/Task7/plot_powerspectrum.m
@@ -15,7 +15,7 @@ ylabel('Powerspectrum / [dim. unit]','Interpreter','LaTeX');
 % axis limits
 xlim([-200,200]);
 
-%%
+%% Frequencies -> cm^-1
 c = 2.99792458*10^8; % speed of light
 
 f1 = 38.91*10^12;
@@ -23,3 +23,21 @@ f2 = 74.77*10^12;
 
 k1 = f1/c * 10^(-2)
 k2 = f2/c * 10^(-2)
+
+%% Task 8 calculate stuff
+
+k = 1600; %N/m
+m = 16 * 1.66053904*10^(-27);
+M = 12 * 1.66053904*10^(-27);
+
+lamda1 = 0
+lambda2 = -k*(-2*m - M)/(m*M)
+lambda3 = k/m
+
+omega1 = 2*sqrt(k/M)*sin(k*pi/(2*(3)));
+omega2 = 2*sqrt(k/m)*sin(k*pi/(2*(3)));
+f1a = omega1/(2*pi);
+f2a = omega2/(2*pi);
+
+k1a = f1a/c * 10^(-2)
+k2a = f2a/c * 10^(-2)
