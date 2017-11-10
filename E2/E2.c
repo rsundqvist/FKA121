db612802adcd311af054404cea66b3ce27baf961
Richard Sundqvist <richard.sundqvist@live.se>
2017-11-10 15:27:34 +0100

    foo

--- E2/E2.c
+++ E2/E2.c
@@ -17,6 +17,10 @@ void calc_acc(double*, double*, int, double);
 
 /* Main program */
 int main() {
+    double m[3];
+    m[0] = 1;
+    m[1] = 2;
+    m[2] = 3;
     // Parameters
     double alpha = 0;
     double dt = 0.1;
@@ -33,42 +37,63 @@ int main() {
     double P[nbr_of_particles]; // discrete
 
     P[0] = sqrt(2*nbr_of_particles);
-    double E_k[5][nbr_of_timesteps];
+    double E_k0[nbr_of_timesteps];
+    double E_k1[nbr_of_timesteps];
+    double E_k2[nbr_of_timesteps];
+    double E_k3[nbr_of_timesteps];
+    double E_k4[nbr_of_timesteps];
 
     double omega[nbr_of_particles];
+
     for (int j = 0; j < nbr_of_particles; j++) {
         omega[j] = 2*sin(j*PI/(nbr_of_particles+1));
     }
 
     foo(Q, q);
     foo(P, p);
-
-    /* timesteps according to velocity Verlet algorithm */
+    // timesteps according to velocity Verlet algorithm
     for (int i = 1; i < nbr_of_timesteps + 1; i++) {
+        
         calc_acc(a, q, nbr_of_particles, alpha);
 
-        /* v(t+dt/2) */
+        // v(t+dt/2)
         for (int j = 0; j < nbr_of_particles; j++) {
             v[j] += dt * 0.5 * a[j];
         } 
 
-        /* q(t+dt) */
+        // q(t+dt) 
         for (int j = 0; j < nbr_of_particles; j++) {
             q[j] += dt * v[j];
         }
 
-        /* a(t+dt) */
+        // a(t+dt)
         calc_acc(a, q, nbr_of_particles, alpha);
 
-        /* v(t+dt) */
+        // v(t+dt)
         for (int j = 0; j < nbr_of_particles; j++) {
             v[j] += dt * 0.5 * a[j];
         }
 
         foo(v, P);
         foo(q, Q);
-        //calc_E_k(omega, P, Q, E_k, nbr_of_particles);
+        
+        calc_E_k(omega, P, Q, E_k0, nbr_of_particles);
+        calc_E_k(omega, P, Q, E_k1, nbr_of_particles);
+        calc_E_k(omega, P, Q, E_k2, nbr_of_particles);
+        calc_E_k(omega, P, Q, E_k3, nbr_of_particles);
+        calc_E_k(omega, P, Q, E_k4, nbr_of_particles);
     }
+
+    
+    //printf("%s", "Print to file: ");
+    //FILE *file = fopen("energy","w");
+    //for (int i = nbr_of_timesteps; i < nbr_of_timesteps; i++) {
+    //    fprintf (file,"%e \t %e \t %e \t %e \t %e \t %e \n", i*dt,
+    //        E_k0[i], E_k1[i], E_k2[i], E_k3[i], E_k4[i]);
+    //}
+    //fclose(file);
+    //printf("Ek.dat created");
+    //printf("m[0] = %e, m[1] = %e, m[2] = %e\n", m[0], m[1], m[2]);
 }
 
 // The formalae for Q_k and P_k are identical with m = 1
@@ -93,6 +118,7 @@ void foo(double *a, double *A)
         A[i] = sum;
     }  
 }
+
 void calc_E_k(double* omega, double* P, double* Q, double* E_k, int size) {
     for (int i = 0; i < size; ++i)
         E_k[i] = 0.5 * (P[i]*P[i] + omega[i]*omega[i]*Q[i]*Q[i]);
@@ -102,7 +128,7 @@ void calc_acc(double *a, double *q, int size_q, double alpha){
     
      // Boundary conditions
     int i = 0;
-    a[i] = q[i+1]-2*q[i]+q[i-1]+alpha*(-q[i+1]+2*q[i]*(q[i+1])); ;
+    a[i] = q[i+1]-2*q[i]+alpha*(-q[i+1]+2*q[i]*(q[i+1])); ;
     i = size_q-1;
     a[i] = 2*q[i]+q[i-1]+alpha*(q[i-1]*q[i-1]+2*q[i]*(-q[i-1]));
 
