f98cbe8fa1df86456e552efb7d38ad385c5c2682
Richard Sundqvist <richard.sundqvist@live.se>
2017-11-16 11:23:48 +0100

    H1.1 framework working

--- H1/MD_main.c
+++ H1/MD_main.c
@@ -12,18 +12,23 @@ int main()
 {
     //========================================================================//
     // Task specific - H1.1
-    //========================================================================//
-    double pos[N][3]; // Position
+    //========================================================================//                         
+    double pos[N][3]; // x, y, z
+    double vel[N][3];
+    double acc[N][3];
     double f[N][3]; // Forces
-    int Nc = 4; // #primitive calls in each direction                           // Segfault sensitive - 4 seems to be about max
-    double lattice[4*Nc*Nc*Nc][3];
+    
     double a0 = 0.1; // Lattice parameter
     double L = 1; // Length of supercell
+    
+    int Nc = 50; // #primitive calls in each direction 
+    double lattice[4*Nc*Nc*Nc][3];
+    init_fcc(lattice, Nc, a0);
 
     //========================================================================//
     // Setup
     //========================================================================//
-    int i, i_log;                                                               // i - actual timestep, i_log - logging of timestep data
+    int i, j, i_log;                                                               // i - actual timestep, i_log - logging of timestep data
     double dt = 0.1;
 	double t_max = 250;
 	int nbr_of_timesteps = t_max/dt;
@@ -42,17 +47,41 @@ int main()
     	if (i%(nbr_of_timesteps/10) == 0) { // Print progress - 10%
     		printf("\tt = %.2f \t\t %.3f  \n", i*dt, ((double)i/nbr_of_timesteps));
     	}
+        //======================================//
+        // Verlet
+        //======================================//
+		for (j = 0; j < N; j++) { // v(t+dt/2)
+		    vel[j][0] += dt * 0.5 * acc[j][0];
+		    vel[j][1] += dt * 0.5 * acc[j][1];
+		    vel[j][2] += dt * 0.5 * acc[j][2];
+		}
+		for (j = 0; j < N; j++) { // q(t+dt)
+		    pos[j][0] += dt * vel[j][0];
+		    pos[j][1] += dt * vel[j][1];
+		    pos[j][2] += dt * vel[j][2];
+		}
+
+        //=====================//
+        // Accelerations
+        //=====================//
+        get_forces_AL(f,pos, L, N);  
+		//calc_acc(a, q, m, kappa, nbr_of_particles); //TODO
+
+		
+		for (j = 0; j < N; j++) { // v(t+dt)
+		    vel[j][0] += dt * 0.5 * acc[j][0];
+		    vel[j][1] += dt * 0.5 * acc[j][1];
+		    vel[j][2] += dt * 0.5 * acc[j][2];
+		}
     	
-    	// Verlet steps
-    	
-    	
+        //====================================================================//
+        // Record data (frequency depends on resolution ir);
+        //====================================================================//
     	if (i%ir == 0) { // Time to log data?
     	    i_log = i/ir;
-            //================================================================//
-            // Log step - record data
-            //================================================================//
+            double energy = get_energy_AL(pos, L, N); // Potential energy
             
-            log_data1[i_log] = 1;
+            log_data1[i_log] = energy;
             log_data2[i_log] = 2;
             log_data3[i_log] = 3;
         }
@@ -65,14 +94,18 @@ int main()
     //====================================================================//
     FILE * file1 = fopen("energy.dat", "w");
     if (file1 != NULL){
-    	printf("Print to file.");
+    	printf("Print to file... ");
 
     	for (i = 0; i < nbr_of_timesteps/ir; i++) { // i are log steps now!
-    	    double t = ir*i*dt;  
-    	    printf("%e \n", t);
+    	    double t = ir*i*dt;
+    	    
+    	    // Print file1
     		fprintf (file1,"%e \t %e \t %e \t %e     \n",
     			t, // Time
     		    log_data1[i], log_data2[i], log_data3[i]); // data
+
+    	    // Print file1
+    	    // ...
     	}
     	
     	
@@ -83,7 +116,7 @@ int main()
     	printf("A file is NULL!\n");
     }
     
-    
+   
     /*
     int bound = 4;
     for (i = -8; i <= 7; i++) {
@@ -114,14 +147,12 @@ int main()
      size N x 3, where N is the number of atoms. The first, second and third column
      correspond to the x,y and z coordinate respectively.
     */
-    init_fcc(lattice, Nc, a0);
     return 1;
     /* 
      Function that calculates the potential energy in units of [eV]. pos should be
      a matrix containing the positions of all the atoms, L is the length of the 
      supercell and N is the number of atoms.
     */
-    double energy;
     energy = get_energy_AL(pos, L, N);
     printf("energy = %.5f", energy);
     
@@ -142,7 +173,6 @@ int main()
      the force resepctively . pos should be a matrix containing the positions of 
      all the atoms, L is the length of the supercell and N is the number of atoms.
     */
-    //get_forces_AL(f,pos, L, N);  
-
+    get_forces_AL(f,pos, L, N);  
     return 0; // Prevent err printout
 }
