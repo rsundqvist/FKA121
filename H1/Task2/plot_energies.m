743cd41a1144d98da852e3cd2e4fd2a38d67a0fc
Anders Hansson <andeh@f7203-20.f7203.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-11-16 18:08:43 +0100

    ekorre

--- H1/Task2/plot_energies.m
+++ H1/Task2/plot_energies.m
@@ -6,8 +6,6 @@ data = load('energy.dat');
 t = data(:,1);
 figure(1);
 hold on;
-plot(t,data(:,2))
-plot(t,data(:,3))
 plot(t,data(:,4))
 hold off;
-legend('Potential energy','Kinetic energy','Total energy')
+
