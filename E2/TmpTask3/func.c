a1e298f60202d87913aa2c3ad05a1c68dfd7e0aa
Anders Hansson <andeh@f7204-16.f7204.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-11-17 11:24:23 +0100

    sdas

--- E2/TmpTask3/func.c
+++ E2/TmpTask3/func.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /*
 E1_func.c
  
@@ -11,18 +13,19 @@ Function that calculates the acceleration based on the Hamiltonian.
 The acceleration is calculated based on the displacements u and then stored in a.
 u and a should be vectors of the same size, size_of_u
 */
-void calc_acc(double *a, double *q, double alpha, int size_of_q)
+void calc_acc(double *a, double *q, double alpha, int sz)
 {
     /* Declaration of variables */
     int i;
-    
+    sz--;
     /* Calculating the acceleration on the boundaries */
-    a[0] = q[1] - q[0] + alpha * (q[1] - q[0])*(q[1] - q[0]);
-    a[size_of_q - 1] = q[size_of_q - 2] - q[size_of_q - 1] - alpha * (q[size_of_q - 1] - q[size_of_q - 2])*(q[size_of_q - 1] - q[size_of_q - 2]);
+    a[0] = q[1] - 2 * q[0] + alpha * (q[1] - q[0])*(q[1] - q[0]) - alpha*q[0]*q[0];
+    a[sz] = q[sz - 1] - 2*q[sz] - alpha * (q[sz] - q[sz-1])* (q[sz] - q[sz-1]) + alpha*q[sz]*q[sz];
     
     /* Calculating the acceleration of the inner points */
-    for (i = 1; i < size_of_q - 1; i++){
+    for (i = 1; i < sz ; i++){
         a[i] = q[i+1] - 2*q[i] + q[i-1] + alpha * (q[i+1]-q[i])* (q[i+1]-q[i]) - alpha*(q[i] - q[i-1])*(q[i] - q[i-1]);
+        //printf("%.5f \n",a[i]);
     }
 }
 
