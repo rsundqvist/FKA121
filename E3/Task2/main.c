0cc8bf08bd44e60f4ab8b14b0144dd567d36817d
Anders Hansson <andeh@f7203-02.f7203.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-11-20 11:31:51 +0100

    task 2 started

--- E3/Task2/main.c
+++ E3/Task2/main.c
@@ -4,10 +4,15 @@
 #include <math.h>
 #include <time.h>
 
+#define PI 3.14159265359
 
 double mathFunction1(double x);
+double mathFunction2(double x);
+double mathFunctionSinPi(double x);
+double mathFunctionArcsinPi(double x);
 void mcIntegrate(double *ans, double (*fuctionPtr)(double), double x, double x2, int N, gsl_rng * q);
-double transformationMethod(double (*invFun)(double), double x1, double x2, gsl_rng g);
+void mcIntegrateImportanceSampling(double *ans, double (*pFun)(double), double (*invFun)(double), double x1, double x2, int N, gsl_rng * q);
+double transformationMethod(double (*invFun)(double), double x1, double x2, gsl_rng * q);
 
 
 gsl_rng * init_rng(); // gsl rng create
@@ -19,8 +24,11 @@ int main() {
 
     double ans[2]; // Return value from mcIntegrate
     int N = 10, i = 1;
-    while(i <= 10) {
-        mcIntegrate(ans, mathFunction1, 0, 1, N, q);
+    // Bounds of inverse cummulative function
+    double x1 = mathFunctionSinPi(0);
+    double x2 = mathFunctionSinPi(1);
+    while(i <= 5) {
+        mcIntegrateImportanceSampling(ans, mathFunction2, mathFunctionArcsinPi, x1, x2, N, q);
         printf("N^%d: \t mu = %.8f, sigma = %.8f \n", i, ans[0], ans[1]);
         N *= 10;
         i++;
@@ -32,6 +40,18 @@ int main() {
 
 double mathFunction1(double x) {
     return x - x*x;
+}
+
+double mathFunction2(double x) {
+    return mathFunction1(x)/mathFunctionSinPi(x);
+}
+
+double mathFunctionSinPi(double x) {
+    return 0.5 * PI * sin(PI*x);
+}
+
+double mathFunctionArcsinPi(double x) {
+    return asin(2 * x/ PI)/PI;
 }
 
 void mcIntegrate(double *ans, double (*fun)(double), double x1, double x2, int N, gsl_rng * q) {
@@ -65,9 +85,31 @@ gsl_rng * init_rng()
 	return q;
 }
 
+void mcIntegrateImportanceSampling(double *ans, double (*gFun)(double), double (*invFun)(double), double x1, double x2, int N, gsl_rng * q) {
+    double rx, y;
+    double ysum = 0, ysqSum = 0;
+    // Sample N points
+    int i;    
+    for(i = 0; i < N; i++) {
+        rx = transformationMethod(invFun, x1,x2,q);
+        y = gFun(rx);
+        ysum += y;
+        ysqSum += y*y;
+    }
+    double mu = ysum/N;
+    double sigmasq = 1/N * ysqSum - mu*mu;
+    if (sigmasq < 0) sigmasq = -sigmasq;
+
+    ans[0] = mu;
+    ans[1] = sqrt(sigmasq/N);
+}
+
+
 // Generate a random value from a continous probability distribution
-double transformationMethod(double (*invFun)(double), double x1, double x2, gsl_rng g) {
-    rx = x1 + (x2-x1)*gsl_rng_uniform(q);
+double transformationMethod(double (*invFun)(double), double x1, double x2, gsl_rng * q) {
+    
+    double rx = x1 + (x2-x1)*gsl_rng_uniform(q);
+    //printf("rx \t %.5f \t %.5f \n",rx, invFun(rx));
     return invFun(rx);
 }
 
