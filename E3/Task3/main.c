6c4d9d4e4fe5bb538fd09f81bf30d8df784ed968
Anders Hansson <andeh@o7152a-03.o7152a.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-11-21 15:38:13 +0100

    task3 e3 updated

--- E3/Task3/main.c
+++ E3/Task3/main.c
@@ -9,6 +9,7 @@
 double mathFunction1(double x);
 double mathFunction2(double x);
 double mathFunction3(double x);
+double mathFunction4(double r[3]);
 double mathFunctionSinPi(double x);
 double mathFunctionArcsinPi(double x);
 void mcIntegrate(double *ans, double (*fuctionPtr)(double), double x, double x2, int N, gsl_rng * q);
@@ -16,9 +17,11 @@ void mcIntegrateImportanceSampling(double *ans, double (*pFun)(double), double (
 double transformationMethod(double (*invFun)(double), double x1, double x2, gsl_rng * q);
 
 //Metropolis
-void metropolisStep(double c[3], double Delta, gsl_rng * q);
+void metropolisStep(double c[3], double ans[3], double Delta, double (*probFunc)(double[3],double[3]), gsl_rng * q);
 double propFunction(double c1[3], double c2[3]);
 double nextCoordinate(double c, double Delta, gsl_rng * q);
+void generateMarkovChain(double (*chain)[3], double (*probFunc)(double[3],double[3]), double Delta, int nbr_of_points, gsl_rng * q);
+void metropolisIntegrate3D(double *ans, double (*gfun)(double[3]), double(*samplePoints)[3], int nbr_of_points);
 
 // gsl stuff
 gsl_rng * init_rng(); // gsl rng create
@@ -27,14 +30,14 @@ gsl_rng * init_rng(); // gsl rng create
 int main() {
 	gsl_rng * q = init_rng();
 
-    double ans[2]; // Return value from mcIntegrate
-    int N = 10, i = 4;
-    // Bounds of inverse cummulative function
-    double x1 = mathFunction3(0);
-    double x2 = mathFunction3(1);
+    double ans[2]; // Return value from integration
+    int N = 10, i = 1;
+    double Delta = 10;
     while(i <= 5) {
-        mcIntegrateImportanceSampling(ans, mathFunction2, mathFunction3, x1, x2, N, q);
-        printf("N^%d: \t mu = %.8f, sigma = %.8f \n", i, ans[0], ans[1]);
+        double markovChain[N][3];
+        generateMarkovChain(markovChain, propFunction, Delta, N, q);
+        metropolisIntegrate3D(ans, mathFunction4, markovChain, N);
+        printf("N=10^%d \t In = %.5f \t sigma = %.5f \n",i,ans[0],ans[1]);
         N *= 10;
         i++;
     }
@@ -57,6 +60,11 @@ double mathFunctionSinPi(double x) {
 
 double mathFunction3(double x) {
     return acos(1 - 2*x)/PI;
+}
+
+double mathFunction4(double r[3]) {
+    double x = r[0], y = r[1], z = r[2];
+    return x*x + x*x*y*y + x*x*y*y*z*z;
 }
 
 void mcIntegrate(double *ans, double (*fun)(double), double x1, double x2, int N, gsl_rng * q) {
@@ -92,17 +100,15 @@ gsl_rng * init_rng()
 
 void mcIntegrateImportanceSampling(double *ans, double (*gFun)(double), double (*invFun)(double), double x1, double x2, int N, gsl_rng * q) {
     double rx, y;
-    double ysum = 0, ysqSum = 0, testsum = 0;
+    double ysum = 0, ysqSum = 0;
     // Sample N points
     int i;    
     for(i = 0; i < N; i++) {
         rx = transformationMethod(invFun, x1,x2,q);
-        testsum += rx;
         y = gFun(rx);
         ysum += y;
         ysqSum += y*y;
     }
-    printf("%.5f \n",testsum/N);
     double mu = ysum/N;
     double sigmasq = 1/N * ysqSum - mu*mu;
     if (sigmasq < 0) sigmasq = -sigmasq;
@@ -121,22 +127,53 @@ double transformationMethod(double (*invFun)(double), double x1, double x2, gsl_
 double nextCoordinate(double c, double Delta, gsl_rng * q) {
     double r = gsl_rng_uniform(q); // [0, 1) - n.b. half-open!
     double cNext = c + Delta*(r - 0.5);
+    return cNext;
 }
 
-void metropolisStep(double c[3], double Delta, gsl_rng * q) {
+void generateMarkovChain(double (*chain)[3], double (*probFunc)(double[3],double[3]), double Delta, int nbr_of_points, gsl_rng * q) {
+    int i;
+    //Randomize start position in [-10,10]
+    for(i=0; i<3; i++)
+        chain[0][i] = gsl_rng_uniform(q);
+    for(i = 0; i< nbr_of_points-1; i++) {
+        metropolisStep(chain[i],chain[i+1] , Delta, probFunc, q);
+    }
+}
+
+void metropolisStep(double c[3], double ans[3], double Delta, double (*probFunc)(double[3],double[3]), gsl_rng * q) {
+    int i;
     double cNext[3];
-    for (int i = 0; i < 3; ++i) {
+    for (i = 0; i < 3; ++i) {
         cNext[i] = nextCoordinate(c[i], Delta, q);
     }
-    double pr = propFunction(c, cNext);
+    double pr = probFunc(cNext, c);
     double r = gsl_rng_uniform(q);
-    if (pr > r) // Accept new step
-        for (int i = 0; i < 3; ++i)
-            c[i] = cNext[i];
+    if (pr >= r) // Accept new step
+        for (i = 0; i < 3; ++i)
+            ans[i] = cNext[i];
+    //printf("%.3f (%.6f,%.6f,%.6f) \n", pr,cNext[0],cNext[1],cNext[2]);
+}
+
+void metropolisIntegrate3D(double *ans, double (*gFun)(double[3]), double(*samplePoints)[3], int nbr_of_points) {
+    double y;
+    double ysum = 0, ysqSum = 0;
+
+    int i;    
+    for(i = 0; i < nbr_of_points; i++) {
+        y = gFun(samplePoints[i]);
+        ysum += y;
+        ysqSum += y*y;
+    }
+    double mu = ysum/nbr_of_points;
+    double sigmasq = 1/nbr_of_points * ysqSum - mu*mu;
+    if (sigmasq < 0) sigmasq = -sigmasq;
+
+    ans[0] = mu;
+    ans[1] = sqrt(sigmasq/nbr_of_points);
 }
 
 double propFunction(double c1[3], double c2[3]){
-    double c1Sq = c1[0]*c1[0] + c1[1]*c1[1] + c1[2]*c1[2];
-    double c2Sq = c2[0]*c2[0] + c2[1]*c2[1] + c2[2]*c2[2];
-    return exp(c1-c2);
+    double c1Sq = -c1[0]*c1[0] - c1[1]*c1[1] - c1[2]*c1[2];
+    double c2Sq = -c2[0]*c2[0] - c2[1]*c2[1] - c2[2]*c2[2];
+    return exp(c2Sq)/exp(c1Sq);
 }
