6a44bde39e3f9a910d181c65fd50afdd278a5a3a
Richard Sundqvist <richard.sundqvist@live.se>
2017-11-22 13:37:43 +0100

    E3.4 import working

--- E3/Task4/main.c
+++ E3/Task4/main.c
@@ -4,6 +4,26 @@
 #include <math.h>
 #include <time.h>
 
+void readValues(const char *url, double * values, unsigned int N);
+
 int main() {
-    double * data = 
+    unsigned int N = 1000000; // 10^6
+    double values[N];
+    readValues("MC.txt", values, N);
+    
+    unsigned int i;
+    for(i = 0; i < 15; i++)
+        printf("%lf\n", values[i]);
+}
+
+void readValues(const char *url, double *values, unsigned int N) {
+    FILE * f = fopen(url, "r");
+    if(f == NULL)
+        printf("Failed to open file: \"%s\"", url);
+ 
+    unsigned int i; 
+    for(i = 0; i < N; ++i) {
+        fscanf(f, "%lf",&values[i]);
+    }
+    close(f);
 }
