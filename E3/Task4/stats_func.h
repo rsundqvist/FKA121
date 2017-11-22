699a03b175fafc1581e69f0310b48925ab849618
Richard Sundqvist <richard.sundqvist@live.se>
2017-11-22 15:34:02 +0100

    e3 screw

--- E3/Task4/stats_func.h
+++ E3/Task4/stats_func.h
@@ -8,4 +8,8 @@ extern double getAheadMean(double * values, int nbr_of_values, int k);
 extern double autoCorrelation(double * values, int nbr_of_values, int k);
 extern double d_abs(double d);
 extern int findS(double * values, int nbr_of_values, double threshold);
+extern double blockAverageS(double * values, int nbr_of_values, int B);
+extern double blockAverage(double * values, int nbr_of_values, int j, int B);
+extern double sampleVariance(double * values, int nbr_of_values);
+extern void setZero(double * arr, int N);
 #endif
