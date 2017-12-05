d637bdb6a9010fc6453089103f6aa220bc73f05e
Anders Hansson <andeh@f7204-04.f7204.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-12-05 12:40:23 +0100

    task3 hw2

---
+++ H2/Task3_One_chain/plot_alphas.m
@@ -0,0 +1,12 @@
+data = load('alphas.dat');
+
+clf;
+hold on
+plot(data(:,1),data(:,2))
+plot(data(:,1),data(:,2)+data(:,3),'--')
+plot(data(:,1),data(:,2)-data(:,3),'--')
+hold off
+
+xlabel('\alpha')
+ylabel('Ground state energy E_0 [Hartree energy]')
+legend('<E_L>','<E_L> + \sigma', '<E_L> - \sigma')
---
+++ H2/Task3_multiple_chains/stat.h
@@ -0,0 +1,10 @@
+
+#ifndef _stat_h
+#define _stat_h
+
+double get_mean(double *vector, int sz);
+double get_variance(double *vector, double mu, int sz);
+
+double get_mean_norm(double (*vector)[3], int sz);
+double get_variance_norm(double (*vector)[3], double mean, int sz);
+#endif
