dd900f6f9aada6adcdb22318a439e43c0feea5ff
Anders Hansson <andeh@f7204-05.f7204.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-12-05 10:15:01 +0100

    h2 task2

--- H2/Task1/blck.m
+++ H2/Task1/blck.m
@@ -7,6 +7,8 @@ t = 15000;
 hold on;
 plot(data(1:t,1), data(1:t,2));
 lim = max(data(1:t,1));
-plot([0 lim], [10 10]);
+plot([0 lim], [12 12]);
 
-legend('\Phi_k', 's = 15');
+xlabel('Block size')
+ylabel('Block average')
+legend('\Phi_k', 's = 12');
