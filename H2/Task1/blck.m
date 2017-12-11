fcdff149f9d5a1052a94a23805139c1245691e20
Richard Sundqvist <whisp91@hotmail.com>
2017-12-11 20:36:27 +0100

    H2 figs

--- H2/Task1/blck.m
+++ H2/Task1/blck.m
@@ -3,16 +3,14 @@ clear all;
 
 data = load('block_average.dat');
 
-s = 10;
+s = 14;
 t = size(data,1);
 hold on;
 plot(data(1:t,1), data(1:t,2));
 lim = max(data(1:t,1));
-plot([0 lim], [12 12]);
 
 xlabel('Block size')
 ylabel('Block average')
-legend('\Phi_k', 's = 12');
-plot([0 t], [s s]);
+plot([2 t+2], [s s], 'Color', 'k', 'LineStyle','-.');
 
 legend('\Phi_k', ['s = ' num2str(s)]);
