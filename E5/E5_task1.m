56d5c4e07f7a60a3526372df0beb4a5114dccf32
Cheezeburger94 <andi199424@hotmail.com>
2017-12-13 11:01:17 +0100

    E5 update

--- E5/E5_task1.m
+++ E5/E5_task1.m
@@ -2,32 +2,41 @@ clear all, close all;
 % Initialize grid
 nbrOfPoints = 10000;
 r0 = 0.001;
-rEnd = 8;
+rEnd = 10;
 rValues = linspace(r0,rEnd,nbrOfPoints); 
 h = rValues(2) - rValues(1); % grid spacing
 
 % Set electron density according to the ground state of hydrogen
-electronDensity = @(r) 1/(pi)*exp(-r);
-ns = electronDensity(rValues);%4 * exp(-2*rValues);
+electronDensity = @(r) exp(-2.*r)/pi;
+% Multiply ns with r, since it will be needed in the equation later
+ns = rValues .* electronDensity(rValues);%4 * exp(-2*rValues);
 
 % Initialize matrix
 A = zeros(nbrOfPoints);
-for i=2:nbrOfPoints-1 % Inner points
+for i=3:nbrOfPoints-2 % Inner points
     A(i,i+1) = 1/(h^2);
     A(i,i-1) = 1/(h^2);
     A(i,i) = - 2/(h^2);
 end
 
 % Boundary conditions
-A(1,1) = -2/(h^2);
+%A(1,1) = -2/(h^2);
+%A(1,2) = 1/(h^2);
+%A(end,end) = -2/(h^2);
+%A(end,end-1) = 1/(h^2);
 A(1,2) = 1/(h^2);
-A(end,end) = -2/(h^2);
+A(2,2) = - 2/(h^2);
+A(2,3) = 1/(h^2);
 A(end,end-1) = 1/(h^2);
+A(end-1, end-1) = - 2/(h^2);
+A(end-1, end-2) = 1/(h^2);
 
 c = zeros(nbrOfPoints,1);
-c(end) = 1/(h^2);
+c(end) = - 2/(h^2);
+c(end-1) = 1/(h^2);
+
 % Set matrix equation Ax = b + c
-U = A\ (ns' + c);
+U = pinv(A)*(- 4*pi*ns' + c);
 
 % Solve Hartree potential and compare to analytical solution
 Vh = @(r) 1./r - (1 + 1./r).*exp(-2*r);
