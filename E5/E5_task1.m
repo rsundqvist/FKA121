408b792a565a649ccadf221b4d08ad38fd9902b7
Anders Hansson <andeh@o7152b-05.o7152b.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-12-13 11:22:18 +0100

    E5 task1 solved

--- E5/E5_task1.m
+++ E5/E5_task1.m
@@ -13,34 +13,34 @@ ns = rValues .* electronDensity(rValues);%4 * exp(-2*rValues);
 
 % Initialize matrix
 A = zeros(nbrOfPoints);
-for i=3:nbrOfPoints-2 % Inner points
+for i=2:nbrOfPoints-1 % Inner points
     A(i,i+1) = 1/(h^2);
     A(i,i-1) = 1/(h^2);
     A(i,i) = - 2/(h^2);
 end
 
 % Boundary conditions
-%A(1,1) = -2/(h^2);
-%A(1,2) = 1/(h^2);
-%A(end,end) = -2/(h^2);
-%A(end,end-1) = 1/(h^2);
+A(1,1) = -2/(h^2);
 A(1,2) = 1/(h^2);
-A(2,2) = - 2/(h^2);
-A(2,3) = 1/(h^2);
+A(end,end) = -2/(h^2);
 A(end,end-1) = 1/(h^2);
-A(end-1, end-1) = - 2/(h^2);
-A(end-1, end-2) = 1/(h^2);
+
 
 c = zeros(nbrOfPoints,1);
-c(end) = - 2/(h^2);
-c(end-1) = 1/(h^2);
+c(end) = - 1/(h^2);
 
 % Set matrix equation Ax = b + c
-U = pinv(A)*(- 4*pi*ns' + c);
+U = A\(- 4*pi*ns' + c);
+Vh_sim = U./rValues';
 
 % Solve Hartree potential and compare to analytical solution
 Vh = @(r) 1./r - (1 + 1./r).*exp(-2*r);
 figure(1)
 hold on
 plot(rValues, Vh(rValues))
-plot(rValues,U./rValues')
+plot(rValues, Vh_sim,'--','Linewidth',3)
+
+legend('Hartree Potential','Simulated Potential')
+title('Electronic potential for Hydrogen')
+xlabel('Distance to nuclues [a_0]')
+ylabel('Hartree energy')
