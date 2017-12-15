77e13537a721099026cbd668992d737914a969c4
Richard Sundqvist <richard.sundqvist@live.se>
2017-12-15 14:55:28 +0100

    h2b.3

--- H2/Task3/plot_alphas2.m
+++ H2/Task3/plot_alphas2.m
@@ -3,15 +3,28 @@ data = load('alphas.dat');
 N = 10^5;
 alphas = data(:,1);
 energy = data(:,2);
-err = data(:,2)/sqrt(N);
+err = ones(size(alphas)) * std(alphas)/sqrt(N);
+
+avg = mean(energy)
+dev = std(energy)
 
 figure;
-plot(alphas, energy);
-ylabel('E [E_{hartree}]');
+hold on
+grid on
+p = polyfit(alphas,energy,2);
+yp = polyval(p, alphas, '-.');
+errorbar(alphas, energy, err);
+%plot(alphas, energy);
+plot(alphas, yp);
+ylabel('E(\alpha) [E_{hartree}]');
 xlabel('\alpha');
-%errorbar(alphas, energy);
 
 Emin = min(energy(:));
 I = find(Emin==energy(:), 1);
 amin = alphas(I)
-title(['E_{min} = ' num2str(Emin) ', \alpha_{min} = ' num2str(amin)]);
+
+I = find(min(yp)==yp(:), 1);
+amin = alphas(I)
+%title(['E_{min} = ' num2str(Emin) ', \alpha_{min} = ' num2str(amin)]);
+title('Energy values');
+legend('Simulation energy', '2nd-degree polynomial fit');
